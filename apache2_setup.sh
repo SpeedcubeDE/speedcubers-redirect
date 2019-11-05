cff91b0195bc63cef701ee395d1f174fad39f343
Felk <de.felix.koenig@gmail.com>
2019-11-05 19:49:49 +0100

    add www subdomain to apache config

--- apache2_setup.sh
+++ apache2_setup.sh
@@ -4,7 +4,7 @@ ln -s "${parent_path}/www" /var/www/speedcubers_redirect
 echo "
 <VirtualHost *:80>
         ServerName speedcubers.de
-        ServerAlias speedcubers.speedcube.de
+        ServerAlias www.speedcubers.de speedcubers.speedcube.de
         DocumentRoot /var/www/speedcubers_redirect
 </VirtualHost>
 " > /etc/apache2/sites-available/speedcubers-redirect.conf
